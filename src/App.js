bc6bd18cfb871e3568d6a6ad1fe94b1e546835c3
Jpgotopo <jpgotopo@GMAIL.COM>
2020-12-23 21:04:13 -0500

    "Agregando Logo y Botón con Bootstrap"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import lexivrit from './lexivrit.svg';
 import './App.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src={lexivrit} className="App-logo" alt="logo" />
+        <h1 className="encabezado">LexIvrit</h1>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <button type="button" class="btn btn-primary">Ir a Léxico</button>
       </header>
+     
     </div>
   );
 }
